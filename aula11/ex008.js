75132d72d5e37bd6b425501338e47d7372e93ae8
Carlos Eduardo Coelho Maia <106678405+ccarlosmaia@users.noreply.github.com>
2022-06-30 20:36:35 -0300

    Delete aula11 directory

--- aula11/ex008.js
+++
@@ -1,6 +0,0 @@
-var vel = 20
-console.log(`A sua velocidade é ${vel}Km/h`)
-if (vel > 60) { //Condição simples
-    console.log('você ultrapassou a velocidade permitida. MULTADO!')
-}
-console.log('Dirija sempre com cinto de segurança!')
