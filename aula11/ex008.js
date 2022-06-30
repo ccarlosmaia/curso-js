a53ecb2e88037ce112acfd82426fca40774cfa40
Carlos Eduardo <c4carlosmaia@gmail.com>
2022-06-30 20:27:49 -0300

    first commit

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 20
+console.log(`A sua velocidade é ${vel}Km/h`)
+if (vel > 60) { //Condição simples
+    console.log('você ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log('Dirija sempre com cinto de segurança!')
